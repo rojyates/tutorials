f4691ea4139b0d9de3ad80582f81a96bc12f31c0
Roger Yates <587230+rojyates@users.noreply.github.com>
2020-03-02 06:04:13 +1000

    BAEL-3603 Removed sysouts

---
+++ io-vs-nio/src/test/java/com/baeldung/java/io/SynchronousIOClientStaticWiremockUnitTest.java
@@ -0,0 +1,57 @@
+package com.baeldung.java.io;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.*;
+import java.net.Socket;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.assertTrue;
+
+public class SynchronousIOClientStaticWiremockUnitTest {
+    private static final String REQUESTED_RESOURCE = "/test.txt";
+
+    @ClassRule public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration
+      .options()
+      .dynamicPort()
+      .bindAddress("localhost"));
+
+    @Rule public WireMockClassRule instanceRule = wireMockRule;
+
+    @Before
+    public void setup() {
+        stubFor(get(urlEqualTo(REQUESTED_RESOURCE)).willReturn(aResponse()
+          .withStatus(200)
+          .withBody("{ \"response\" : \"It worked!\" }\r\n\r\n")));
+    }
+
+    @Test
+    public void whenGetUsingIO_thenUseStreams() throws IOException {
+        Socket socket = new Socket("localhost", instanceRule.port());
+        try (InputStream serverInput = socket.getInputStream();
+          BufferedReader reader = new BufferedReader(new InputStreamReader(serverInput));
+          OutputStream clientOutput = socket.getOutputStream();
+          PrintWriter writer = new PrintWriter(new OutputStreamWriter(clientOutput))) {
+            writer.print("GET " + REQUESTED_RESOURCE + " HTTP/1.0\r\n\r\n");
+            writer.flush(); // important - without this the request is never sent, and the test will hang on readLine()
+
+            StringBuilder ourStore = new StringBuilder();
+
+            String line = reader.readLine();
+            while (line != null) {
+                ourStore.append(line);
+                line = reader.readLine();
+            }
+            System.out.println("Our store has length: " + ourStore.length());
+            System.out.println("Our store contains: '" + ourStore.toString() + "'");
+
+            assertTrue(ourStore.length() > 0);
+            assertTrue(ourStore.toString().contains("It worked!"));
+        }
+    }
+}
