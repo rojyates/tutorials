8d6616a5f5bead70bf7bdc444cac6f26bef874fa
Roger Yates <587230+rojyates@users.noreply.github.com>
2020-03-02 06:04:13 +1000

    BAEL-3603 Removed sysouts

---
+++ io-vs-nio/src/test/java/com/baeldung/java/io/AsynchronousIOClientStaticWiremockUnitTest.java
@@ -0,0 +1,89 @@
+package com.baeldung.java.io;
+
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.Assert.assertTrue;
+
+public class AsynchronousIOClientStaticWiremockUnitTest {
+    private String REQUESTED_RESOURCE = "/test.json";
+
+    @ClassRule public static WireMockClassRule wireMockRule = new WireMockClassRule(WireMockConfiguration
+      .options()
+      .dynamicPort()
+      .bindAddress("localhost"));
+
+    @Rule public WireMockClassRule instanceRule = wireMockRule;
+
+    @Before
+    public void setup() {
+        stubFor(get(urlEqualTo(REQUESTED_RESOURCE)).willReturn(aResponse()
+          .withStatus(200)
+          .withBody("{ \"response\" : \"It worked!\" }")));
+    }
+
+    @Test
+    public void whenGetUsingNIO_thenUseBuffers() throws IOException {
+        InetSocketAddress address = new InetSocketAddress("localhost", instanceRule.port());
+        SocketChannel socketChannel = SocketChannel.open(address);
+        Charset charset = Charset.forName("UTF-8");
+        socketChannel.write(charset.encode(CharBuffer.wrap("GET " + REQUESTED_RESOURCE + " HTTP/1.0\r\n\r\n")));
+
+        ByteBuffer buffer = ByteBuffer.allocate(8192); // allocateDirect for direct memory access
+        CharBuffer charBuffer = CharBuffer.allocate(8192);
+        CharsetDecoder decoder = charset.newDecoder();
+        StringBuilder ourStore = new StringBuilder();
+        while (socketChannel.read(buffer) != -1 || buffer.position() > 0) {
+            buffer.flip();
+            storeBufferContents(buffer, charBuffer, decoder, ourStore);
+            buffer.compact();
+        }
+        socketChannel.close();
+        assertTrue(ourStore.length() > 0);
+        assertTrue(ourStore.toString().contains("It worked!"));
+    }
+
+    @Test
+    public void whenGetUsingNIO_thenUseSmallBuffers() throws IOException {
+        InetSocketAddress address = new InetSocketAddress("localhost", instanceRule.port());
+        SocketChannel socket = SocketChannel.open(address);
+        Charset charset = Charset.forName("UTF-8");
+        socket.write(charset.encode(CharBuffer.wrap("GET " + REQUESTED_RESOURCE + " HTTP/1.0\r\n\r\n")));
+
+        ByteBuffer buffer = ByteBuffer.allocate(8); // allocateDirect for direct memory access
+        CharBuffer charBuffer = CharBuffer.allocate(8);
+        CharsetDecoder decoder = charset.newDecoder();
+        StringBuilder ourStore = new StringBuilder();
+        while (socket.read(buffer) != -1 || buffer.position() > 0) {
+            buffer.flip();
+            storeBufferContents(buffer, charBuffer, decoder, ourStore);
+            buffer.compact();
+        }
+        socket.close();
+        assertTrue(ourStore.length() > 0);
+        assertTrue(ourStore.toString().contains("It worked!"));
+    }
+
+    void storeBufferContents(ByteBuffer buffer, CharBuffer charBuffer, CharsetDecoder decoder, StringBuilder ourStore) {
+        decoder.decode(buffer, charBuffer, true);
+        charBuffer.flip();
+        ourStore.append(charBuffer);
+        charBuffer.clear();
+    }
+
+}
